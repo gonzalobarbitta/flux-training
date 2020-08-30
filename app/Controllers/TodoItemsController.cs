5b3188d7b0d8d45ac69f731dc9160c2c50d31077
Gonzalo Barbitta <gonzalo.barbitta@bnasdev.onmicrosoft.com>
2020-08-30 20:33:41 -0300

    Updated items in controller

--- app/Controllers/TodoItemsController.cs
+++ app/Controllers/TodoItemsController.cs
@@ -14,10 +14,11 @@ namespace CloudSample.App.Web.Controllers
             return new[]
             {
                 new TodoItem("Install prerequisites", true),
-                new TodoItem("Create the flux namespace", true),
+                new TodoItem("Create flux namespace", true),
                 new TodoItem("Install flux", true),
                 new TodoItem("Give write access on GitHub repository", true),
                 new TodoItem("Demo"),
+                new TodoItem("Review integration with Helm Operator"),
             };
         }
     }
