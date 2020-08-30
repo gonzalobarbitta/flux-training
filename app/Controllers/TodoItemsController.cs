d41c56cc53e94c3f73b339d793b5d52af38ecd21
Gonzalo Barbitta <gonzalo.barbitta@bnasdev.onmicrosoft.com>
2020-08-30 16:20:26 -0300

    Added .net core sample application

---
+++ app/Controllers/TodoItemsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CloudSample.App.Web.Models;
+
+namespace CloudSample.App.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoItemsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            return new[]
+            {
+                new TodoItem("Install prerequisites", true),
+                new TodoItem("Create the flux namespace", true),
+                new TodoItem("Install flux", true),
+                new TodoItem("Give write access on GitHub repository", true),
+                new TodoItem("Demo"),
+            };
+        }
+    }
+}
---
+++ app/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿namespace CloudSample.App.Web.Models
+{
+    public class TodoItem
+    {
+        public TodoItem(string name, bool isComplete = false)
+        {
+            Name = name;
+            IsComplete = isComplete;
+        }
+
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
